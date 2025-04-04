8d00526225c5c8d0a09d57d218a44263f1de5724
4thidiot <tendingtowardsinfinity@gmail.com>
2025-04-04 18:03:00 +0530

    Create B - Odd Grasshopper.cpp

---
+++ 900/B - Odd Grasshopper.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    ll x, n;
+	    cin>>x>>n;
+	    
+	    ll add=0;
+	    
+	   if (n%4 == 0)
+	       add = 0;
+	   else if (n%4 == 1)      
+	       add = -1*n;
+	   else if (n%4 == 2)          
+	       add = 1;
+	   else
+	       add = n+1;
+	       
+	   if (abs(x)%2 == 1)    //Why it wont work without the abs for negative numbers?
+	        add *= -1;
+	   
+	   cout<<x+add<<endl;     
+	}
+}
